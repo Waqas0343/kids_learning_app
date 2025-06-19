7a8c8b28c9e0fbce17657ca2351bde8eb6dd53a0
mwaqas <waqasraza0343@gmail.com>
2025-06-19 15:11:48 +0500

    Remove Non Use Code

--- lib/routes/app_routes.dart
+++ lib/routes/app_routes.dart
@@ -1,20 +1,20 @@
 class AppRoutes {
-  static const String splash = '/splash';
-  static const String homeScreen = '/homeScreen';
-  static const String drawingScreen = '/drawingScreen';
-  static const String graphicsScreen = '/graphicsScreen';
-  static const String characterScreen = '/characterScreen';
-  static const String drawCharacterScreen = '/drawCharacterScreen';
-  static const String numbersScreen = '/numbersScreen';
-  static const String colorsScreen = '/colorsScreen';
-  static const String shapesScreen = '/shapesScreen';
-  static const String animalScreen = '/animalScreen';
-  static const String birdsScreen = '/birdsScreen';
-  static const String drawNumberScreen = '/drawNumberScreen';
-  static const String colorFillingScreen = '/colorFillingScreen';
-  static const String drawShapesScreen = '/drawShapesScreen';
-  static const String drawAnimalScreen = '/drawAnimalScreen';
-  static const String kidsLearnTableScreen = '/kidsLearnTableScreen';
+  static  String splash = '/splash';
+  static  String homeScreen = '/homeScreen';
+  static  String drawingScreen = '/drawingScreen';
+  static  String graphicsScreen = '/graphicsScreen';
+  static  String characterScreen = '/characterScreen';
+  static  String drawCharacterScreen = '/drawCharacterScreen';
+  static  String numbersScreen = '/numbersScreen';
+  static  String colorsScreen = '/colorsScreen';
+  static  String shapesScreen = '/shapesScreen';
+  static  String animalScreen = '/animalScreen';
+  static  String birdsScreen = '/birdsScreen';
+  static  String drawNumberScreen = '/drawNumberScreen';
+  static  String colorFillingScreen = '/colorFillingScreen';
+  static  String drawShapesScreen = '/drawShapesScreen';
+  static  String drawAnimalScreen = '/drawAnimalScreen';
+  static  String kidsLearnTableScreen = '/kidsLearnTableScreen';
 
 }
 
