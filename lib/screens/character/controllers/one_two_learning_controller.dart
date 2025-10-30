5c8517c8941bf414bc3bf60f8f86145ea805a875
mwaqas <waqasraza0343@gmail.com>
2025-10-30 17:41:36 +0500

    Implement One Two

--- lib/screens/character/controllers/one_two_learning_controller.dart
+++ lib/screens/character/controllers/one_two_learning_controller.dart
@@ -1,11 +1,63 @@
 import 'package:get/get.dart';
+import 'package:flutter_tts/flutter_tts.dart';
 
-class OneTwoLearningController extends GetxController{
+class OneTwoLearningController extends GetxController {
+  final FlutterTts flutterTts = FlutterTts();
+  var numberWords = <String>[].obs;
 
+  @override
+  void onInit() {
+    super.onInit();
+    generateNumberWords();
+  }
+
+  void generateNumberWords() {
+    List<String> words = [];
+    for (int i = 1; i <= 500; i++) {
+      words.add(numberToWords(i));
+    }
+    numberWords.assignAll(words);
+  }
+
+  Future<void> speak(String text) async {
+    await flutterTts.speak(text);
+  }
+
+  String numberToWords(int number) {
+    final ones = [
+      "", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"
+    ];
+    final teens = [
+      "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen",
+      "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"
+    ];
+    final tens = [
+      "", "", "Twenty", "Thirty", "Forty", "Fifty",
+      "Sixty", "Seventy", "Eighty", "Ninety"
+    ];
+
+    if (number < 10) {
+      return ones[number];
+    } else if (number < 20) {
+      return teens[number - 10];
+    } else if (number < 100) {
+      int ten = number ~/ 10;
+      int one = number % 10;
+      return "${tens[ten]}${one > 0 ? " ${ones[one]}" : ""}";
+    } else if (number < 1000) {
+      int hundred = number ~/ 100;
+      int remainder = number % 100;
+      String hundredPart = "${ones[hundred]} Hundred";
+      if (remainder == 0) return hundredPart;
+      return "$hundredPart and ${numberToWords(remainder)}";
+    } else {
+      return "Five Hundred";
+    }
+  }
 
   @override
   void onClose() {
-    // TODO: implement onClose
+    flutterTts.stop();
     super.onClose();
   }
 }
--- lib/screens/character/one_two_learning.dart
+++ lib/screens/character/one_two_learning.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:kids_learning_app/app_helpers/app_spacing.dart';
 import 'controllers/one_two_learning_controller.dart';
 
 class OneTwoLearning extends StatelessWidget {
@@ -9,9 +10,95 @@ class OneTwoLearning extends StatelessWidget {
   Widget build(BuildContext context) {
     final OneTwoLearningController controller = Get.put(OneTwoLearningController());
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
-        title: const Text("Learning Counting "),
+        title:  Text(
+          "Let's Learn Counting!",
+          style: Get.textTheme.titleSmall?.copyWith(
+            fontWeight: FontWeight.bold,
+            fontSize: 22,
+            color: Colors.white,
+          )
+        ),
+        backgroundColor: Colors.orange.shade600,
+        elevation: 6,
+        centerTitle: true,
       ),
+      body: Obx(() {
+        return GridView.builder(
+          padding: const EdgeInsets.all(16),
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            mainAxisExtent: 200,
+            mainAxisSpacing: 16,
+            crossAxisSpacing: 16,
+          ),
+          itemCount: controller.numberWords.length,
+          itemBuilder: (context, index) {
+            final word = controller.numberWords[index];
+            final number = index + 1;
+            return GestureDetector(
+              onTap: () => controller.speak(word),
+              child: AnimatedContainer(
+                duration: const Duration(milliseconds: 300),
+                curve: Curves.easeOut,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(20),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.orange.withOpacity(0.2),
+                      blurRadius: 8,
+                      offset: const Offset(0, 5),
+                    ),
+                  ],
+                  border: Border.all(color: Colors.orange.shade300, width: 2),
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    CircleAvatar(
+                      radius: 30,
+                      backgroundColor: Colors.orange.shade400,
+                      child: Text(
+                          "$number",
+                          style: Get.textTheme.titleSmall?.copyWith(
+                            fontSize: 22,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                          )
+                      ),
+                    ),
+                    otherSpacerVertically(),
+                    Text(
+                      word,
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.w600,
+                        color: Colors.purple.shade700,
+                      ),
+                    ),
+                    otherSpacerVertically(),
+                    Container(
+                      padding: const EdgeInsets.all(6),
+                      decoration: BoxDecoration(
+                        color: Colors.orange.shade400,
+                        shape: BoxShape.circle,
+                      ),
+                      child: const Icon(
+                        Icons.volume_up,
+                        color: Colors.white,
+                        size: 20,
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            );
+          },
+        );
+      }),
     );
   }
 }
