67de92463047e6f3184b029998858a859acc9126
mwaqas <waqasraza0343@gmail.com>
2025-10-31 10:14:06 +0500

    Done with J

--- lib/screens/character/character_wise_object.dart
+++ lib/screens/character/character_wise_object.dart
@@ -11,7 +11,7 @@ class CharacterWiseObject extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final controller = Get.put(CharacterWiseObjectController());
+    final CharacterWiseObjectController controller = Get.put(CharacterWiseObjectController());
 
     return Scaffold(
       appBar: AppBar(
